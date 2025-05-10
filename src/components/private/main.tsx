c53e5586c4f3289956989e73bc8b8c020a31fd04
Matthew Morrison <james.morrison@asurion.com>
2025-05-10 16:14:35 -0500

    add space at the bottom

--- src/components/private/main.tsx
+++ src/components/private/main.tsx
@@ -2,7 +2,7 @@ import * as elements from 'typed-html';
 
 export function Main({ children, ...rest }: elements.Attributes) {
   return (
-    <main class="px-lg lg:px-2xl flex flex-col" {...rest}>
+    <main class="mb-xl px-lg lg:px-2xl flex flex-col" {...rest}>
       {children}
     </main>
   );
